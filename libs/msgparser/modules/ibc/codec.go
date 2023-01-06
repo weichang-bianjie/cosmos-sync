4c7973fdddc600640e465220509bd4d6359ac2a2
huangweichang <weichang@bianjie.ai>
2023-01-06 21:55:02 +0800

    add  ics721transfer codec

--- libs/msgparser/modules/ibc/codec.go
+++ libs/msgparser/modules/ibc/codec.go
@@ -2,6 +2,7 @@ package ibc
 
 import (
 	"github.com/bianjieai/cosmos-sync/libs/msgparser/codec"
+	ics721transfer "github.com/bianjieai/nft-transfer"
 	ibctransfer "github.com/cosmos/ibc-go/v5/modules/apps/transfer"
 	ibc "github.com/cosmos/ibc-go/v5/modules/core"
 )
@@ -10,5 +11,6 @@ func init() {
 	codec.RegisterAppModules(
 		ibc.AppModuleBasic{},
 		ibctransfer.AppModuleBasic{},
+		ics721transfer.AppModuleBasic{},
 	)
 }
